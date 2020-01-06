9ff7e09e631c02889336d2bf8951a84470ce6a04
Matthew Hahn <matthewhahn73@gmail.com>
2020-01-05 19:21:30 -0600

    Update AutoRun.bat

--- AutoRun.bat
+++ AutoRun.bat
@@ -3,7 +3,7 @@ cd "C:\Users\matth\Documents\Scripts\Python\Web-Scraper>"
 set p1=-url "https://tinyurl.com/yftrmbu7"
 set p2=-kwor "java" "python" "sql" "c++" "programmer" "junior" "jr" "entry" "intern"
 set p3=-sms
-set p4=-ph="5732754140"
-set p5=-e="genericmail157@gmail.com"
-set p6=-p="Vanity157$"
+set p4=-ph=""
+set p5=-e=""
+set p6=-p=""
 python -u Main.py %p1% %p2% %p3% %p4% %p5% %p6%
