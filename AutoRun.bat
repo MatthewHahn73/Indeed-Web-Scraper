e924dd354c3d92b046d705c43c6b334409c836be
Matthew Hahn <matthewhahn73@gmail.com>
2020-01-22 19:40:54 -0600

    Update AutoRun.bat

--- AutoRun.bat
+++ AutoRun.bat
@@ -1,9 +1,9 @@
 @ECHO off
-cd "C:\Users\matth\Documents\Scripts\Python\Web-Scraper>"
-set p1=-url "https://tinyurl.com/yftrmbu7"
-set p2=-kwor "java" "python" "sql" "c++" "programmer" "junior" "jr" "entry" "intern"
+cd "<Full path to web-scraper main>"
+set p1=-url "<indeed url to scrape>"
+set p2=-kwor "<Keyword 1>" "<Keyword 2>" "<etc..>"
 set p3=-sms
-set p4=-ph=""
-set p5=-e=""
-set p6=-p=""
+set p4=-ph="<Phone number to send info to>"
+set p5=-e="<Host email address>"
+set p6=-p="<Host email password>"
 python -u Main.py %p1% %p2% %p3% %p4% %p5% %p6%
